f26ab28a475892e20639fb186265609329de7e37
Dandadanc <cito.daniele.21@itisriva.edu.it>
2025-03-17 16:04:44 +0100

    Lavorazione progetto
    Progressione del progetto.

--- Esercizio array/Program.cs
+++ Esercizio array/Program.cs
@@ -7,10 +7,54 @@ using System.Threading.Tasks;
 namespace Esercizio_array
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ciao");
+            // Daniele Cito 3I3 17/3/2025
+            int scelta;
+            int[] numeri = { 10, 20, 30, 40, 50 };
+            do
+            {
+                Console.WriteLine("\n--- MENU ---");
+                Console.WriteLine("1. Visualizza array");
+                Console.WriteLine("2. Cerca numero");
+                Console.WriteLine("3. Esci");
+                Console.Write("Scegli un'opzione: ");
+                scelta = int.Parse(Console.ReadLine());
+
+                if (scelta == 1)
+                {
+                    for (int i = 0; i < numeri.Length; i++)
+                        Console.Write(numeri[i] + " ");
+                    Console.WriteLine();
+                }
+                else if (scelta == 2)
+                {
+                    Console.Write("Inserisci numero da cercare: ");
+                    int valore = int.Parse(Console.ReadLine());
+
+                    bool trovato = false;
+                    for (int i = 0; i < numeri.Length; i++)
+                    {
+                        if (numeri[i] == valore)
+                        {
+                            trovato = true;
+                            break;
+                        }
+                    }
+
+                    if (trovato)
+                        Console.WriteLine("Numero trovato!");
+                    else
+                        Console.WriteLine("Numero non presente.");
+                }
+                else if (scelta != 3)
+                {
+                    Console.WriteLine("Opzione non valida!");
+                }
+            } while (scelta != 3);
+            Console.WriteLine("Programma terminato.");
         }
     }
 }
